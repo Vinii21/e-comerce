429039df76e0a9017e715c731b32a59bbcac5a72
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-04-12 17:11:30 -0600

    Footer concluido

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
 const Footer = () => {
     return (
         <footer>
-            
+            <a href="https://github.com/Vinii21" target="_blank"><i className='bx bxl-github'></i> Vinicio Chavarría</a>
+            <a href="https://github.com/luis1290" target="_blank"><i className='bx bxl-github'></i> Luis Gómez</a>
+            <a href="https://github.com/ronald1710" target="_blank"><i className='bx bxl-github'></i> Ronald Azofeifa</a>
+            <span>2023</span>
         </footer>
     );
 }
